6f683cd8eab889567b7bfe2e27922ae38b4c9b1e
sova <mahatoshobha143@gmail.com>
2023-07-14 22:44:55 +0545

    modified

--- tinder_swipable_cards/lib/home_page.dart
+++ tinder_swipable_cards/lib/home_page.dart
@@ -15,9 +15,8 @@ class _HomePageState extends State<HomePage> {
 
   List ListOfImg=[
    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3OB57LjKTpekAVYvEbc8cjFJvzBenrPixSg&usqp=CAU",
-               "https://i.pinimg.com/736x/15/32/8b/15328b7692973f352be5ea6224936cfa.jpg"
-           "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg",
-            "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg",
+  "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg",
+  "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg",
 
   ];
   @override
@@ -27,7 +26,8 @@ class _HomePageState extends State<HomePage> {
         //mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           SwipeableCardsSection(
-            cardHeightBottomMul: 0.9,
+            // cardHeightMiddleMul : 0.3,
+            // cardHeightBottomMul: 0.1,
             cardController: _cardController,
             context: context,
             //add the first 3 cards (widgets)
@@ -35,11 +35,7 @@ class _HomePageState extends State<HomePage> {
               TinderCard(img: "https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2021/01/14101943/New-Featured-1-3.jpg?tr=w-1200,h-900"),
               TinderCard(img: "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg"),
               TinderCard(img: "https://www.ctvnews.ca/polopoly_fs/1.5036379.1595530668!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg"),
-              // TinderCard(img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3OB57LjKTpekAVYvEbc8cjFJvzBenrPixSg&usqp=CAU"),
-              // TinderCard(img: "https://i.pinimg.com/736x/15/32/8b/15328b7692973f352be5ea6224936cfa.jpg"),
-              // TinderCard(img: "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg"),
-              // TinderCard(img: "https://myrepublica.nagariknetwork.com/uploads/media/1540801500_box1_20200205140033.jpg"),
-
+             
               
             ],
             onCardSwiped: (dir, index, widget) {
